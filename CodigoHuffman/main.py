df900c6e54664f270517ef0e21e44b1fd2e9e0ee
Iasmin Torres <iasmin.torres04@gmail.com>
2024-07-08 14:35:16 -0300

    alterações na main

--- CodigoHuffman/main.py
+++ CodigoHuffman/main.py
@@ -7,15 +7,5 @@ class Main:
         menu = Menu()
         menu.executar()
 
-        # Exemplo de descompressão de arquivo após o uso do menu
-        huffman = Huffman()  # Crie uma instância da classe Huffman
-        arquivo_entrada = "saida.huf"  # Substitua pelo nome do seu arquivo de entrada .huf
-        arquivo_saida = "entrada.txt"  # Substitua pelo nome desejado para o arquivo de saída .txt
-
-        texto_original = Huffman.descomprimir_arquivo(arquivo_entrada, arquivo_saida)
-
-        print("Texto original descomprimido:")
-        print(texto_original)
-
 if __name__ == "__main__":
     Main.main()
